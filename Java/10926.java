7b3243537348bed507409730f1addc29ececa439
sojeong630 <62346198+sojeong630@users.noreply.github.com>
2020-05-03 22:53:29 +0900

    10296_??!
    백준 10296번 문제풀이

---
+++ Java/10926.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String id;
+		
+		Scanner in = new Scanner(System.in);
+		
+		id = in.nextLine();
+		System.out.println(id+"??!");
+	}
+
+}
