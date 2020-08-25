b29d408c744d86587f7ab1c040b8976120280ead
sojeong630 <62346198+sojeong630@users.noreply.github.com>
2020-08-25 22:58:27 +0900

    11022_A+B-8
    백준 11022번 문제풀이

---
+++ Java/11022_A+B-8.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		int T = sc.nextInt();
+		
+		for(int i = 1; i <= T; i++) {
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			System.out.println("Case #"+i+": "+x+" + "+y+" = "+(x+y));
+			
+		}
+		sc.close();
+	}
+
+}
